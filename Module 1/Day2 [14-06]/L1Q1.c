49869771bbaf0146976e0a231cc2e324240ca3de
rinit12 <rinithd12@gmail.com>
2023-06-16 16:42:17 +0530

    Day2

---
+++ Module 1/Day2 [14-06]/L1Q1.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void printExponent(double *x) {
+    unsigned long long *ptr = (unsigned long long *)x;  
+
+    
+    unsigned long long exponent = (*ptr >> 52) & 0x7FF;
+
+   
+    printf("Exponent in hexadecimal: 0x%llx\n", exponent);
+
+   
+    printf("Exponent in binary: 0b");
+    for (int i = 10; i >= 0; i--) {
+        unsigned long long bit = (exponent >> i) & 1;
+        printf("%llu", bit);
+    }
+    printf("\n");
+}
+
+int main() {
+    double x = 0.7;
+    printExponent(&x);
+
+    return 0;
+}
